5efee71d923fb3e4bdfb3ac9be5e6efa4e91fabf
levina <leda@advers.ru>
2025-07-18 21:31:07 +0400

    api_conditioners

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ErrorBoundaryFilter } from './utils/error-boundary.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api_conditioners');
   app.useGlobalFilters(new ErrorBoundaryFilter());
   await app.listen(8000);
 }
